1ffc3192ee0387c31d4a054c2876833c795ba303
fourthcafe <fourthcafe@gmail.com>
2017-07-31 02:36:54 +0900

    다이아몬드 찍기

---
+++ Ch01/170730Exercise/Diamond.cpp
@@ -0,0 +1,37 @@
+//
+// Created by fourthcafe on 2017. 7. 31..
+//
+
+#include <iostream>
+
+
+// maxWidth :: 가로 행의 길이
+// writeCnt :: 찍을 *의 갯수
+void writeRow(int spaceCnt, int writeCnt) {
+	for (int j = 1; j <= spaceCnt; j++) {
+		std::cout << " ";
+	}
+
+	for (int j = 1; j <= writeCnt; j++) {
+		std::cout << "*";
+	}
+
+	std::cout << std::endl;
+}
+
+void writeDiamond(int row) {
+	int height = 2 * row - 1;
+
+	for (int i = 1; i <= height; i++) {
+		if (i <= row) {
+			writeRow(row - i, 2 * i - 1);
+		} else {
+			writeRow(i - row, 2 * (2 * row - i) - 1);
+		}
+	}
+}
+
+
+int main() {
+	writeDiamond(10);
+}
