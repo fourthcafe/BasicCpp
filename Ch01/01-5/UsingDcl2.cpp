39fec347245ad877b39fb0b8ba27f38c49674432
fourthcafe <fourthcafe@gmail.com>
2017-07-31 23:56:01 +0900

    using 키워드 사용법

---
+++ Ch01/01-5/UsingDcl2.cpp
@@ -0,0 +1,23 @@
+//
+// Created by fourthcafe on 2017-07-31.
+//
+
+//using 키워드는 선언 범위 내에서만 효력을 발휘한다.
+//전역 범위에서 사용하는 방법
+//cin, cout, endl 만으로 함수의 사용이 가능하다.
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	int num = 20;
+	cout << "Hello world!" << endl;
+	cout << "Hello " << "world" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+
+	return 0;
+}
