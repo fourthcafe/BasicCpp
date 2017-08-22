feb7bacf399fc7f0433d5a2bbc4b2f7186e6e49b
fourthcafe <fourthcafe@gmail.com>
2017-08-23 02:28:33 +0900

    question02-3

--- Ch02/02-5/NewObject.cpp
+++ Ch02/02-5/NewObject.cpp
@@ -2,7 +2,7 @@
 // Created by fourthcafe on 2017. 8. 22..
 //
 
-//Simple이라는 자료형의 변수를 하나 할당하는 문장인데 실행결과가 다르다.
+// Simple이라는 자료형의 변수를 하나 할당하는 문장인데 실행결과가 다르다.
 // new 와 malloc 함수의 동작방식에는 차이가 있다.
 
 #include <iostream>
@@ -17,13 +17,17 @@ public:
 };
 
 int main() {
+	// case1. cpp 방식 변수 할당
 	cout << "case 1: ";
 	Simple *sp1 = new Simple;
 
+	// case2. c 방식 변수 할당
 	cout << "case 2: ";
 	Simple *sp2 = (Simple *) malloc(sizeof(Simple) * 1);
 
 	cout << endl << "end of main" << endl;
+
+	// 할당방법에 따른 소멸 진행
 	delete sp1;
 	free(sp2);
 	return 0;
