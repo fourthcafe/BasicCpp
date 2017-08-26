e0c8dc2363306c55cd1b7dc96c19fa058c28427f
fourthcafe <fourthcafe@gmail.com>
2017-08-26 18:51:56 +0900

    const pointer의 이해

--- Ch02/02-5/PointerNConst.cpp
+++ Ch02/02-5/PointerNConst.cpp
@@ -9,34 +9,34 @@
 
 int main() {
 	// string 은 std library에 포함되어 있음
-	std::string nickname = "fourthcafe";
+	std::string nickName = "fourthcafe";
 	std::string address = "seoul";
 
 
 	// 포인터가 가리키고 있는 값을 변경할 수 없도록 한다.
-	const std::string *myname1 = &address;
-
-	// 에러, 가리키는 값은 변경불가.
-//	*myname1 = "faceChecker";
-	// 통과, 가리키는 주소는 변경가능.
-	myname1 = &nickname;
+	const std::string *constStringPointer = &nickName;
+	// 가리키는 주소는 변경가능.
+	constStringPointer = &address;
 
 
 	// 포인터가 가리키고 있는 주소를 변경할 수 없도록 한다. (참조자와 유사)
-	std::string *const myname2 = &address;
+	std::string *const stringPointerConst = &nickName;
 
-	// 통과, 가리키는 값은 변경가능.
-	*myname2 = "푸우";
-	// 에러, 가리키는 주소는 변경불가.
-//	myname2 = &nickname;
+	// 에러! 가리키는 주소는 변경불가
+//	stringPointerConst = &address;
+	// 가리키는 값은 변경가능.
+	*stringPointerConst = "faceChecker";
 
 
 	// 포인터가 가리키고 있는 주소와 값 모두 변경할 수 없도록 한다.
-	const std::string *const myname = &address;
+	const std::string *const constStringPointerConst = &nickName;
 
 	// 에러, 가리키는 값은 변경불가.
-//	*myname1 = "푸우";
+//	*constStringPointerConst = "푸우";
 	// 에러, 가리키는 주소도 변경불가.
-	myname1 = &nickname;
+//	*constStringPointerConst = &nickname;
+
+	std::string const *stringConstPointer = &nickName;
+	stringConstPointer = &address;
 }
 
